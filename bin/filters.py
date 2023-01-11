9e4a276510d150713f53031087c13302d9056804
cyendes@environmentalworks.com <cyendes@environmentalworks.com>
2023-01-11 13:11:54 -0600

    exercise finished

--- bin/filters.py
+++ bin/filters.py
@@ -11,5 +11,11 @@ def apply_filter(file: object, filter: str) -> object:
     4. Convert the PIL Image object to file object
     5. Return the file object
     """
+    image = Image.open(file)
+    image = image.filter(eval(f"ImageFilter.{filter.upper()}"))
 
-    pass
+    file = io.BytesIO()
+    image.save(file,"JPEG")
+    file.seek(0)
+    
+    return file
